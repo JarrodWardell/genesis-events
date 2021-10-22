2bbed533eeb4fc55d14c0f453de26c78a97e7d99
Abbass-Ali Siwjee <abu.siwjee@utoronto.ca>
2021-10-21 21:36:29 -0400

    Update sorting for tournament rounds

--- web/src/components/PrintRound/PrintRound.js
+++ web/src/components/PrintRound/PrintRound.js
@@ -6,55 +6,58 @@ class PrintRound extends React.Component {
       <div className="print flex">
         {round &&
           round.matches &&
-          round?.matches?.map((match, index) => (
-            <div
-              className="py-4 border-t-2 border-b-2 border-dashed border-black"
-              key={match.id}
-            >
-              <h1 className="text-center w-full mb-1 text-xl">
-                {tournament.name} - Round {round.roundNumber} - Print Sheet
-              </h1>
+          [...round?.matches]
+            .sort((a, b) => b.players.length - a.players.length)
+            .map((match, index) => (
+              <div
+                className="py-4 border-t-2 border-b-2 border-dashed border-black"
+                key={match.id}
+              >
+                <h1 className="text-center w-full mb-1 text-xl">
+                  {tournament.name} - Round {round.roundNumber} - Print Sheet
+                </h1>
 
-              <h2 className="text-center w-full mb-1 text-md">
-                {new Date().toDateString()}
-              </h2>
+                <h2 className="text-center w-full mb-1 text-md">
+                  {new Date().toDateString()}
+                </h2>
 
-              <table className="w-full">
-                <tr>
-                  <th className="py-2 text-center">Table #</th>
-                  <th className="py-2 text-center">Player 1 Nickname</th>
-                  <th className="py-2 text-center">Player 1 Score</th>
-                  <th className="py-2 text-center">Player 1 Signature</th>
-                  <th className="py-2 text-center">Player 2 Nickname</th>
-                  <th className="py-2 text-center">Player 2 Score</th>
-                  <th className="py-2 text-center">Player 2 Signature</th>
-                </tr>
-                <tr>
-                  <td className="mr-2 text-center">{index + 1}</td>
-                  {match?.players?.map((playerScore) => (
-                    <>
-                      <td className="py-2 text-center">
-                        {playerScore?.user?.nickname || playerScore.playerName}
-                      </td>
+                <table className="w-full">
+                  <tr>
+                    <th className="py-2 text-center">Table #</th>
+                    <th className="py-2 text-center">Player 1 Nickname</th>
+                    <th className="py-2 text-center">Player 1 Score</th>
+                    <th className="py-2 text-center">Player 1 Signature</th>
+                    <th className="py-2 text-center">Player 2 Nickname</th>
+                    <th className="py-2 text-center">Player 2 Score</th>
+                    <th className="py-2 text-center">Player 2 Signature</th>
+                  </tr>
+                  <tr>
+                    <td className="mr-2 text-center">{index + 1}</td>
+                    {match?.players?.map((playerScore) => (
+                      <>
+                        <td className="py-2 text-center">
+                          {playerScore?.user?.nickname ||
+                            playerScore.playerName}
+                        </td>
 
-                      <td className="py-2 text-center font-bold">
-                        {playerScore?.bye ? (
-                          'BYE'
-                        ) : playerScore.score ? (
-                          <span>{playerScore.score}</span>
-                        ) : (
+                        <td className="py-2 text-center font-bold">
+                          {playerScore?.bye ? (
+                            'BYE'
+                          ) : playerScore.score ? (
+                            <span>{playerScore.score}</span>
+                          ) : (
+                            <div className="ml-4 border-b border-black w-20 h-4" />
+                          )}
+                        </td>
+                        <td className="py-2">
                           <div className="ml-4 border-b border-black w-20 h-4" />
-                        )}
-                      </td>
-                      <td className="py-2">
-                        <div className="ml-4 border-b border-black w-20 h-4" />
-                      </td>
-                    </>
-                  ))}
-                </tr>
-              </table>
-            </div>
-          ))}
+                        </td>
+                      </>
+                    ))}
+                  </tr>
+                </table>
+              </div>
+            ))}
       </div>
     )
   }
